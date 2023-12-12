fe4cb92dc2c9db72b2393e49ff4ad8734f40512d
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2023-12-12 09:56:21 -0500

    Assignment 8

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,82 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  String message = "";
+  boolean upPressed = false;
+  boolean downPressed = false;
+  boolean leftPressed = false;
+  boolean rightPressed = false;
+
+  float circleX = 150;
+  float circleY = 150;
+
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(51,204,255);
   }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+  if (mousePressed) {
+    fill(102,255,102);
+    ellipse(mouseX, mouseY, 25, 25);
   
-  // define other methods down here.
+    textSize(100);
+    text(message, 50, 180);
+  }
+  if (upPressed) {
+      circleY--;
+    }
+  if (downPressed) {
+      circleY++;
+    }
+  if (leftPressed) {
+      circleX--;
+    }
+  if (rightPressed) {
+      circleX++;
+    }
+  
+    ellipse(circleX, circleY, 30, 30);
+  }
+
+  public void mouseClicked() {
+    fill(255, 128, 0);
+    ellipse(mouseX-10, mouseY-30, 30, 30);
+    ellipse(mouseX-10, mouseY-50, 30,30);
+    ellipse(mouseX-30, mouseY-30, 30,30);
+    ellipse(mouseX-30, mouseY-50, 30,30);
+
+    fill(255, 0, 0);
+    ellipse(mouseX-20, mouseY-40, 30,30);
+  }
+
+  public void keyTyped() {
+      message += key;
+  }
+
+  public void keyPressed() {
+    if (keyCode == UP) {
+      upPressed = true;
+    }else if (keyCode == DOWN) {
+      downPressed = true;
+    }else if (keyCode == LEFT) {
+      leftPressed = true;
+    }else if (keyCode == RIGHT) {
+      rightPressed = true;
+    }
+    }
+      
+  public void keyReleased() {
+    if (keyCode == UP) {
+      upPressed = false;
+    }else if (keyCode == DOWN) {
+      downPressed = false;
+    }else if (keyCode == LEFT) {
+      leftPressed = false;
+    }else if (keyCode == RIGHT) {
+      rightPressed = false;
+    }
+  }
 }
